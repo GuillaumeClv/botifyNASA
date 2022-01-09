2ef2e2a0614b64b7269c09b380cf7ebf0eb46dba
Guillaume Claveau <guillaumeclaveau@hotmail.fr>
2022-01-09 10:58:22 +0100

    import data in app

--- botify-nasa/src/App.js
+++ botify-nasa/src/App.js
@@ -1,8 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './style/App.css';
+import dataJson from './data.json'
 
 function App() {
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    setData(dataJson.near_earth_objects)
+  }, [setData])
+
   return (
-    <div>hello</div>
+    <ul>
+      {data.map(item => {
+        console.log(item)
+        return<li key={item.id}>
+          {item.estimated_diameter.kilometers.estimated_diameter_min}
+        </li>
+      })}
+    </ul>
   )
 }
 
